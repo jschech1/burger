14465001c58af59ab15456f04949ffb7de068769
Jordan Schechter <jordanwschechter@gmail.com>
2018-09-15 17:29:52 -0600

    incomplete push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Buger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Onion Burger', true);
